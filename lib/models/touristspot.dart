d57579b97f570d0a09f1aa8a6d856c26ec25565e
Souvik <souvikde.ns@gmail.com>
2021-02-28 17:10:04 +0530

    Create touristspot.dart

---
+++ lib/models/touristspot.dart
@@ -0,0 +1,32 @@
+class TouristSpot {
+  String id;
+  String name;
+  String description;
+  String bestTime;
+  List<String> images;
+  String state;
+  double rating;
+  List<String> type;
+
+  TouristSpot(
+      {this.id,
+      this.name,
+      this.description,
+      this.bestTime,
+      this.images,
+      this.rating,
+      this.state,
+      this.type});
+
+  factory TouristSpot.fromJson(Map<String, dynamic> json) {
+    return TouristSpot(
+        id: json['id'],
+        name: json['name'],
+        description: json['description'],
+        bestTime: json['bestTime'],
+        images: json['images'],
+        rating: json['rating'],
+        state: json['state'],
+        type: json['type']);
+  }
+}
