634503eb9fce43df103a680e4383fa78e7e1c981
Souvik <souvikde.ns@gmail.com>
2021-03-12 18:38:53 +0530

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: null,
+      body: Query(
+        options: QueryOptions(document: gql("""query {
+  allTouristSpots {
+    id
+    name
+    description
+    bestTime
+    images
+    state
+    rating
+    type
+  }
+} """)),
+        builder: (QueryResult result,
+            {VoidCallback refetch, FetchMore fetchMore}) {
+          return Text(result.toString());
+        },
+      ),
     );
   }
 }
