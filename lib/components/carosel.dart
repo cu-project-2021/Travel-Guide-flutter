a76892274b187ad94fdcba92ab0d776bf46b4bbb
Souvik <souvikde.ns@gmail.com>
2021-03-15 23:05:11 +0530

    Update carosel.dart

--- lib/components/carosel.dart
+++ lib/components/carosel.dart
@@ -12,12 +12,12 @@ class SideCarosel extends StatelessWidget {
         scrollDirection: Axis.horizontal,
         itemCount: spots.length,
         itemBuilder: (context, index) {
+          print(spots[index].images[0]);
           return Container(
               padding: EdgeInsets.all(7),
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(10),
-                child: Image.network(
-                    "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"),
+                child: Image.network(spots[index].images[0]),
               ));
         },
       ),
