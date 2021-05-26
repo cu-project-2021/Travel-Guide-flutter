3fa0241d438f537d1511f63ca036eb6a4df3a1de
Souvik <souvikde.ns@gmail.com>
2021-05-26 23:26:20 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:learn_flutter/grahql.dart';
 import 'package:learn_flutter/screens/home_screen.dart';
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
